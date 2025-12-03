90df249c9790aaf5747c6e5a2960d862b6db82d6
rocha985 <arkhanticsbr@outlook.com>
2025-12-03 05:51:31 -0300

    routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,10 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+/**
+ * @var RouteCollection $routes
+ */
+
 $routes->get("/", "Home::index");
 
 $routes->match(["get", "post"], "/login", "Login::index");
